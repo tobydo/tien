4e6895c63804532f90ea4e9947cc54e308da4537
emischaufeld <eschaufeld@gmail.com>
2020-03-03 16:56:17 -0500

    emi sceme and camera switch

---
+++ tien/Assets/scripts/CameraSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera cameraOne;
+    public Camera cameraTwo;
+    public bool cameraOneOn = true;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOne.enabled = true;
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W) && cameraOneOn ==false){
+            cameraOne.enabled = true;
+            cameraTwo.enabled = false;
+            cameraOneOn = true;
+        }
+        if(Input.GetKeyDown(KeyCode.D) && cameraOneOn ==true){
+            cameraOne.enabled = false;
+            cameraTwo.enabled = true;
+            cameraOneOn = false;
+        }
+    }
+
+}
