a040e299d067fde7ea2591a242b29ef5c96ced40
emischaufeld <eschaufeld@gmail.com>
2020-04-01 20:07:50 -0400

    tmp

--- tien/Assets/scripts/StateMachineTest.cs
+++ tien/Assets/scripts/StateMachineTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 using UnityEngine.UI;
 
@@ -9,7 +10,7 @@ public class StateMachineTest : MonoBehaviour
     public TextAsset Scene;
         string [] lines;
     string  speaker;
-    public Text talking;
+    public TextMeshProUGUI talking;
     int currentLine;
     public enum Characters{
         Tien,
